40589c4b0a352284374273a865e0aaaa08aa3e34
Nusky173 <simon50.lecordier@gmail.com>
2023-09-18 18:51:15 +0200

    start carroussel

--- component/carroussel/carroussel.tsx
+++ component/carroussel/carroussel.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import styles from "./carroussel.module.css"
+
 const Carroussel = () => {
+
+    const imgTab = [
+        "../../public/38830.PNG",
+        "../../public/40169.PNG",
+        "../../public/eye-udjat-EG.PNG"     
+    ]
+
+    
+
     return (
-        <div>
-            <img src=""/>
+        <div className={styles.carroussel}>
+            {imgTab.length > 0 && 
+                imgTab.map((e, index ) => {
+                    let className = 'img' + index
+                    console.log(className)
+                    return (
+                        <div className={styles[className]} key={index}/>
+                    );
+                })
+            }
         </div>
     );
 }
