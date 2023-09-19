b133ad18de0125d84771dd3f7a41a5c0fd0535e0
Nusky173 <simon50.lecordier@gmail.com>
2023-09-19 14:57:13 +0200

    carroussel end

--- component/carroussel/carroussel.tsx
+++ component/carroussel/carroussel.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./carroussel.module.css"
+import { useEffect, useState } from "react";
 
 const Carroussel = () => {
 
@@ -9,19 +12,44 @@ const Carroussel = () => {
         "../../public/eye-udjat-EG.PNG"     
     ]
 
-    
+    const [activeIndex, setActiveIndex] = useState(0);
+    let newIndex = 0;
+
+    const updateIndex = () => {
+        if(activeIndex +1 < imgTab.length) {
+            newIndex++
+        } else {
+            newIndex = 0
+        }
+
+        setActiveIndex(newIndex);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            updateIndex()
+            console.log(activeIndex)
+        }, 3000);
+
+        return () => {
+            if(interval) {  
+                clearInterval(interval)
+            }
+        }
+    });
 
     return (
-        <div className={styles.carroussel}>
+        <div className={styles.carroussel} >
+        <div className={styles.inner} style={{transform: `translateX(-${activeIndex * 100}%)`}}>
             {imgTab.length > 0 && 
                 imgTab.map((e, index ) => {
                     let className = 'img' + index
-                    console.log(className)
                     return (
-                        <div className={styles[className]} key={index}/>
+                        <div className={`${styles[className]} ${styles.carroussel_item}`} key={index} style={{width: "100%"}}></div>
                     );
                 })
             }
+        </div>
         </div>
     );
 }
